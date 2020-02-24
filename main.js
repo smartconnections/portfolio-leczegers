4d4264cf834a5a7fc5e6560135bb811f6c2362ca
leczegers <60180288+leczegers@users.noreply.github.com>
2020-02-24 19:24:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+      $(menu-toggler).removeClass('open');
+      $('.top-nav').removeClass('open');
+    });
+    
+   
+    
+});
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 40px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
